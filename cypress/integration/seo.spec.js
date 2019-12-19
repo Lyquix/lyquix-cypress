2e46c22ef5afef9acf89cd945eb98ff812deb900
Ruben Reyes <ruben.reyes@lyquix.com>
2019-12-19 14:08:30 -0500

    Add: tests for opengraph tags and favicons

--- cypress/integration/seo.spec.js
+++ cypress/integration/seo.spec.js
@@ -37,11 +37,34 @@ options.testUrls.forEach((testUrl) => {
 		});
 
 		it('Meta description', () => {
-			cy.get('meta[name="description"]').should('have.length', 1);
+			cy.get('meta[name="description"]').should('exist').should('have.length', 1);
 		});
 
-		it('Favicon', () => {
-			cy.get('link[rel="shortcut icon"]').should('have.length', 1);
+		it('OpenGraph', () => {
+			cy.get('meta[property="og:title"]').should('exist').should('have.length', 1);
+			cy.get('meta[property="og:description"]').should('exist').should('have.length', 1);
+			cy.get('meta[property="og:image"]').should('exist');
+		});
+
+		it('Favicons', () => {
+			[
+				'link[rel="shortcut icon"][type="image/vnd.microsoft.icon"]',
+				'link[rel="apple-touch-icon"][sizes="57x57"]',
+				'link[rel="apple-touch-icon"][sizes="60x60"]',
+				'link[rel="apple-touch-icon"][sizes="72x72"]',
+				'link[rel="apple-touch-icon"][sizes="76x76"]',
+				'link[rel="apple-touch-icon"][sizes="114x114"]',
+				'link[rel="apple-touch-icon"][sizes="120x120"]',
+				'link[rel="apple-touch-icon"][sizes="144x144"]',
+				'link[rel="apple-touch-icon"][sizes="152x152"]',
+				'link[rel="apple-touch-icon"][sizes="180x180"]',
+				'link[rel="icon"][type="image/png"][sizes="192x192"]',
+				'link[rel="icon"][type="image/png"][sizes="32x32"]',
+				'link[rel="icon"][type="image/png"][sizes="96x96"]',
+				'link[rel="icon"][type="image/png"][sizes="16x16"]',
+			].forEach(function(sel) {
+				cy.get(sel).should('have.length', 1);
+			});
 		});
 
 	});
