3f6055c870bd52778f1072fd432b97ac9248f2fd
Ruben Reyes <ruben.reyes@lyquix.com>
2019-12-19 14:08:49 -0500

    Add: tests for accordions and tabs

---
+++ cypress/integration/accordions.spec.js
@@ -0,0 +1,79 @@
+/// <reference types="Cypress" />
+// Prevent js error "resizeobserver loop limit" from breaking cypress tests
+Cypress.on('uncaught:exception', (err) => {
+  if(/^ResizeObserver loop limit exceeded/.test(err.message)) {
+    return false
+  }
+});
+
+const options = require('../fixtures/options');
+
+let visitOptions = {};
+if('httpUser' in options && options.httpUser != '') visitOptions.auth = {
+	username: options.httpUser,
+	password: options.httpPassword
+};
+
+options.testUrls.forEach((testUrl) => {
+	describe('Accordions on ' + testUrl, function() {
+		before(() => {
+			cy.visit(testUrl, visitOptions);
+		});
+		Object.keys(options.screens).forEach((size) => {
+			it(size, function() {
+				cy.viewport(options.screens[size][0], options.screens[size][1]);
+				cy.wait(250);
+				if(Cypress.$('.accordion-group').length) {
+					cy.get('.accordion-group').each(function(accordionGroupElem, accordionGroupIndex, accordionGroupList) {
+						if(Cypress.$(accordionGroupList).is(':visible')) {
+							// Open and then close each accordion
+							cy.wrap(accordionGroupElem).children('.accordion').each(function(clickElem, clickIndex) {
+								// Open a closed accordion
+								cy.wrap(clickElem).should('have.class', 'closed');
+								//cy.wrap(clickElem).children('.accordion-content').children().should('not.be.visible');
+								cy.wrap(clickElem).children('.accordion-header').trigger('click');
+								cy.wrap(clickElem).should('have.class', 'open');
+								cy.wrap(clickElem).children('.accordion-content').should('be.visible');
+								cy.wrap(accordionGroupElem).children('.accordion').each(function(otherElem, otherIndex) {
+									if(clickIndex != otherIndex) {
+										cy.wrap(otherElem).should('have.class', 'closed');
+										//cy.wrap(otherElem).children('.accordion-content').children().should('not.be.visible');
+									}
+								});
+								// Close the accordion
+								cy.wrap(clickElem).children('.accordion-header').trigger('click');
+								cy.wrap(clickElem).should('have.class', 'closed');
+								//cy.wrap(clickElem).children('.accordion-content').children().should('not.be.visible');
+								cy.wrap(accordionGroupElem).children('.accordion').each(function(otherElem, otherIndex) {
+									if(clickIndex != otherIndex) {
+										cy.wrap(otherElem).should('have.class', 'closed');
+										//cy.wrap(otherElem).children('.accordion-content').should('not.be.visible');
+									}
+								});
+							});
+							// Open one accordion then jump to the next
+							cy.wrap(accordionGroupElem).children('.accordion').each(function(clickElem, clickIndex) {
+								// Open a closed accordion
+								cy.wrap(clickElem).should('have.class', 'closed');
+								//cy.wrap(clickElem).children('.accordion-content').should('not.be.visible');
+								cy.wrap(clickElem).children('.accordion-header').trigger('click');
+								cy.wrap(clickElem).should('have.class', 'open');
+								cy.wrap(clickElem).children('.accordion-content').should('be.visible');
+								cy.wrap(accordionGroupElem).children('.accordion').each(function(otherElem, otherIndex) {
+									if(clickIndex != otherIndex) {
+										cy.wrap(otherElem).should('have.class', 'closed');
+										//cy.wrap(otherElem).children('.accordion-content').should('not.be.visible');
+									}
+								});
+							});
+							// Close the last open accordion
+							cy.get('.accordion.open .accordion-header').trigger('click');
+						}
+						else cy.expect(true).to.be.true;
+					});
+				}
+				else cy.expect(true).to.be.true;
+			});
+		});
+	});
+});
---
+++ cypress/integration/tabs.spec.js
@@ -0,0 +1,51 @@
+/// <reference types="Cypress" />
+// Prevent js error "resizeobserver loop limit" from breaking cypress tests
+Cypress.on('uncaught:exception', (err) => {
+  if(/^ResizeObserver loop limit exceeded/.test(err.message)) {
+    return false
+  }
+});
+
+const options = require('../../fixtures/options');
+
+let visitOptions = {};
+if('httpUser' in options && options.httpUser != '') visitOptions.auth = {
+	username: options.httpUser,
+	password: options.httpPassword
+};
+
+options.testUrls.forEach((testUrl) => {
+	describe('Accordions on ' + testUrl, function() {
+		before(() => {
+			cy.visit(testUrl, visitOptions);
+		});
+		Object.keys(options.screens).forEach((size) => {
+			it(size, function() {
+				cy.viewport(options.screens[size][0], options.screens[size][1]);
+				cy.wait(250);
+				if(Cypress.$('.tab-nav').length && Cypress.$('.tab-content').length) {
+					cy.get('.tab-nav').each(function(tabNavElem, tabNavIndex, tabNavList) {
+						if(Cypress.$(tabNavList).is(':visible')) {
+							// Click on each tab
+							cy.wrap(tabNavList).children('.tab').each(function(tabElem, tabIndex, tabList) {
+								cy.wrap(tabElem).trigger('click');
+								cy.wrap(tabList). each(function(otherTab, otherIndex) {
+									var tabNumber = Cypress.$(otherTab).attr('data-number');
+									if(tabIndex == otherIndex) {
+										cy.wrap(otherTab).should('have.class', 'open');
+										cy.get('.tab-content .tab-panel[data-number="' + tabNumber + '"]').should('have.class', 'open').should('be.visible');
+									}
+									else {
+										cy.wrap(otherTab).should('have.class', 'closed');
+										cy.get('.tab-content .tab-panel[data-number="' + tabNumber + '"]').should('have.class', 'closed').should('not.be.visible');
+									}
+								})
+							});
+						}
+						else cy.expect(true).to.be.true;
+					});
+				}
+			});
+		});
+	});
+});
